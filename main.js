d48b3b613046e55ca35230b932ae78505c4dcb7e
Devin Archer <devin.archer@live.ca>
2022-10-23 02:43:24 -0600

    Seems to be working basically as intended, but still some minor fixes ahead.

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-
 var parentContainer = document.getElementById("parentContainer");
-var columns = 16;
-var rows = 16;
+createGrid(16,16);
 
-var grid = document.createElement('div');
-grid.className = 'grid';
-for (var i = 0; i < columns; ++i) {
-    var column = document.createElement('div'); // create column
-    column.className = 'column';
-    for (var j = 0; j < rows; ++j) {
-        var row = document.createElement('div'); // create row
-        row.className = 'row';
-        row.textContent = i + '-' +j; // set text
-        column.appendChild(row); // append row in column
+function createGrid(c,r){
+    var columns = c;
+    var rows = r;
+    var grid = document.createElement('div');
+    grid.className = 'grid';
+    for (var i = 0; i < columns; ++i) {
+        var column = document.createElement('div'); // create column
+        column.className = 'column';
+        for (var j = 0; j < rows; ++j) {
+            var row = document.createElement('div'); // create row
+            row.className = 'row';
+            row.textContent = i + '-' +j; // set text
+            column.appendChild(row); // append row in column
+        }
+        grid.appendChild(column); // append column inside grid
     }
-    grid.appendChild(column); // append column inside grid
+    parentContainer.appendChild(grid);
 }
-parentContainer.appendChild(grid);
 
-/* Set up a “hover” effect so that the grid divs change color when your mouse passes over them, 
-leaving a (pixelated) trail through your grid like a pen would.
-Hint: “Hovering” is what happens when your mouse enters a div and ends when
- your mouse leaves it. You can set up event listeners for either of those events as a 
- starting point. There are multiple ways to change the color of the divs, including:
-adding a new class to the div. changing the div’s background color using JavaScript.*/
+function getVariable(){
+    var newColumn = window.prompt("size?");
+    createGrid(newColumn, newColumn);
+}
 
+var btn = document.getElementById("resetBtn").addEventListener("click", function() {
+    getVariable();
+});
 
